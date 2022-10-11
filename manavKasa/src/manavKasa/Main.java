884d1b44b5756c5f70b3f55b1edd9c1cb9d0f9ec
MBilcanoglu <72490880+MBilcanoglu@users.noreply.github.com>
2022-10-11 21:32:35 +0300

    Add files via upload
    Kullanıcıların manavdan almış oldukları ürünlerin kilogram değerlerine göre toplam tutarını ekrana yazdıran program.

---
+++ manavKasa/src/manavKasa/Main.java
@@ -0,0 +1,28 @@
+package manavKasa;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double armut,elma,domates,muz,patlican;
+		double tutar=0;
+		
+		Scanner input=new Scanner(System.in);
+		
+		System.out.print("Armut Kaç Kilo: ");
+		armut=input.nextDouble();
+		System.out.print("Elma Kaç Kilo: ");
+		elma=input.nextDouble();
+		System.out.print("Domates Kaç Kilo: ");
+		domates=input.nextDouble();
+		System.out.print("Muz Kaç Kilo: ");
+		muz=input.nextDouble();
+		System.out.print("Patlýcan Kaç Kilo: ");
+		patlican=input.nextDouble();
+		
+		tutar=(armut*2.14)+(elma*3.67)+(domates*1.11)+(muz*0.95)+(patlican*5);
+		System.out.println("Toplam Tutar: "+tutar+" TL");
+	}
+
+}
